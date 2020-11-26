5d56ddd57a26727991f56bae07c0e517e4e6cefc
Ophelie_Marot <ophelie.marot@gmail.com>
2020-11-26 13:05:13 +0100

    form

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,11 +4,12 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class PageController extends AbstractController
+class PagesController extends AbstractController
 {
     /**
      * @Route("/articles", name="articles_list")
@@ -43,72 +44,87 @@ class PageController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/article/insert-static", name="article-insert-static")
-     */
+    ///**
+     //* @Route("/article/insert-static", name="article-insert-static")
+     //*/
 
     //Je créée une nouvelle methode qui utlislise la fonction 'EntityManagerInterface'
-    public function insertStaticArticle(EntityManagerInterface $entityManager)
-    {
+    //public function insertStaticArticle(EntityManagerInterface $entityManager)
+    //{
         //Puis j'instancie dans une variable qui contient ma table article, et qui feras donc le lien avec ma BDD
-        $article = new article();
+        //$article = new article();
 
         //Puis je rajoute tout mes éléments qui seront envoyés dans ma BDD
-        $article->setTitle("Paris");
-        $article->setContent("Paris est la ville la plus peuplée et la capitale de la France.
-    Elle se situe au cœur d'un vaste bassin sédimentaire aux sols fertiles et au climat tempéré, le bassin parisien,
-    sur une boucle de la Seine, entre les confluents de celle-ci avec la Marne et l'Oise. Paris est également le chef-lieu
-    de la région Île-de-France et le centre de la métropole du Grand Paris, créée en 2016. Elle est divisée 
-    en arrondissements, comme les villes de Lyon et de Marseille, au nombre de vingt. Administrativement, la ville 
-    constitue depuis le 1er janvier 2019 une collectivité à statut particulier nommée « Ville de Paris »
-    (auparavant, elle était à la fois une commune et un département). L'État y dispose de prérogatives particulières 
-    exercées par le préfet de police de Paris. La ville a connu de profondes transformations sous le Second Empire dans 
-    les décennies 1850 et 1860 à travers d'importants travaux consistant notamment au percement de larges avenues,
-    places et jardins et la construction de nombreux édifices, dirigés par le baron Haussmann, donnant à l'ancien Paris
-    médiéval le visage qu'on lui connait aujourd'hui.");
-        $article->setPicture("https://upload.wikimedia.org/wikipedia/commons/c/c6/Tour_eiffel_paris-eiffel_tower.jpg");
-        $article->setDate(new \DateTime());
-        $article->setPublish(true);
+        //$article->setTitle("Paris");
+        //$article->setContent("Paris est la ville la plus peuplée et la capitale de la France.
+    //Elle se situe au cœur d'un vaste bassin sédimentaire aux sols fertiles et au climat tempéré, le bassin parisien,
+    //sur une boucle de la Seine, entre les confluents de celle-ci avec la Marne et l'Oise. Paris est également le chef-lieu
+    //de la région Île-de-France et le centre de la métropole du Grand Paris, créée en 2016. Elle est divisée
+    //en arrondissements, comme les villes de Lyon et de Marseille, au nombre de vingt. Administrativement, la ville
+    //constitue depuis le 1er janvier 2019 une collectivité à statut particulier nommée « Ville de Paris »
+    //(auparavant, elle était à la fois une commune et un département). L'État y dispose de prérogatives particulières
+    //exercées par le préfet de police de Paris. La ville a connu de profondes transformations sous le Second Empire dans
+    //les décennies 1850 et 1860 à travers d'importants travaux consistant notamment au percement de larges avenues,
+    //places et jardins et la construction de nombreux édifices, dirigés par le baron Haussmann, donnant à l'ancien Paris
+    //médiéval le visage qu'on lui connait aujourd'hui.");
+        //$article->setPicture("https://upload.wikimedia.org/wikipedia/commons/c/c6/Tour_eiffel_paris-eiffel_tower.jpg");
+        //$article->setDate(new \DateTime());
+        //$article->setPublish(true);
 
         //Je pré enregistre en faisant un 'persist', équivalant de 'commit' dans Git
-        $entityManager->persist($article);
+        //$entityManager->persist($article);
 
         //Puis un Flush qui est mon équivalant de 'push'
-        $entityManager->flush();
+        //$entityManager->flush();
 
         //Puis je retourne ma requête instancié par mon controller, ma reponse HTTP vers mon navigateur, par le biait
         //de mon fichier twig
-        return $this->render('article/insert_static.html.twig');
+        //return $this->render('article/insert_static.html.twig');
 
-    }
+    //}
 
     //Je créée une nouvelle route, un nouveau lien pour modifier mes titres
-    /**
-     * @Route("article/update-static/{id}", name="update_static")
-     */
+    ///**
+     //* @Route("article/update-static/{id}", name="update_static")
+     //*/
 
     //Puis ma fonction avec mes focntions pour faire le lien avec ma bdd et ma wildcard 'id'
-    public function updateStaticArticle(ArticleRepository $articleRepository, EntityManagerInterface $entityManager,$id)
-    {
+    //public function updateStaticArticle(ArticleRepository $articleRepository, EntityManagerInterface $entityManager,$id)
+    //{
         //Je stocke dans une caribale ma fonction pour faire le lien avec ma bdd ainsi que la ma wilcard "$id"
-        $article = $articleRepository->find($id);
+        //$article = $articleRepository->find($id);
 
         //Je modifie mon titre
-        $article->setTitle("Ma ville");
+        //$article->setTitle("Ma ville");
 
         //je fais mon persite équilant de commit
-        $entityManager->persist($article);
+        //$entityManager->persist($article);
         //et mon flush équivalant de push
-        $entityManager->flush();
+        //$entityManager->flush();
 
         //Pour finir je fais appelle a ma fonction render pour retourner ma reponse en http par le bié de mon fichier twig
-        return $this->render('article/update_static.html.twig');
+        //return $this->render('article/update_static.html.twig');
+    //}
+    /**
+     * @Route("/article/insert", name="article_insert")
+     */
+
+    public function insertArticle()
+    {
+        $form = $this->createForm(ArticleType::class);
+
+        $formView = $form->createView();
+
+        return $this->render('article/insert.html.twig',[
+
+            'formView' => $formView
+            ]);
     }
 
     /**
      * @Route("/article/delete/{id}", name="article_delete")
      */
-    //Je créée unne nouvelle methode pour supprimer un article avec ma wildCard, et j'instencie dans une viariable mes fonctions
+     //Je créée unne nouvelle methode pour supprimer un article avec ma wildCard, et j'instencie dans une viariable mes fonctions
     public function deleteArticle($id,ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
     {
         //je stocke dans une variable mon appel a mes "id" dans ma bdd
@@ -123,7 +139,7 @@ class PageController extends AbstractController
             $this->addFlash(
                 "success",
                 "Ton article à bien été supprimé
-                        Bien ouèj !!"
+                        //Bien ouèj !!"
             );
         }
 
@@ -131,5 +147,6 @@ class PageController extends AbstractController
         return $this->redirectToRoute("articles_list");
 
     }
+
 
 }
