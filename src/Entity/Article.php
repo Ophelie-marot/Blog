870dec167963c08c6584102558eedd69ca029fed
Ophelie_Marot <ophelie.marot@gmail.com>
2020-11-30 11:02:52 +0100

    Assert symfony

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -20,26 +21,58 @@ class Article
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(
+     *     message="Merci de remplir le titre"
+     * )
+     *
+     * @Assert\Length(
+     *     min=4,
+     *     max=50,
+     *     minMessage="Pas assez de caractères",
+     *     maxMessage="Trop de caractères"
+     * )
+     *
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank (
+     *     message="Merci de remplir le contenu"
+     * )
+     *
+     * @Assert\Length(
+     *     min=4,
+     *     max=500,
+     *     minMessage="Pas assez de caractères",
+     *     maxMessage="Trop de caractères"
+     * )
+     *
+     *
      */
     private $content;
 
     /**
      * @ORM\Column(type="string")
+     *
      */
     private $picture;
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\Date(
+     *     message="Merci de remplir une dâte"
+     * )
      */
     private $date;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Assert\NotNull(
+     *     message="Merci de spécifier si l'article doit être publié ou non"
+     * )
+     *
      */
     private $publish;
 
